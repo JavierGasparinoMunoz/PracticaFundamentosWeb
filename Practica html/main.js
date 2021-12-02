013b28fb95abed4cf6d4fabca88546e932bcd830
JavierGasparinoMunoz <47026676+JavierGasparinoMunoz@users.noreply.github.com>
2021-12-02 02:06:29 +0100

    Add files via upload

---
+++ Practica html/main.js
@@ -0,0 +1,126 @@
+$(document).ready(function(){
+
+
+    $('#nombre').neonText({
+        textColor:'#ffffff',
+        textSize:'40pt',
+        neonHighlight:'#000dff',
+        neonHighlightColor:'#fc0505',
+        neonHighlightHover:'#99cdcf',
+        neonFontHover:'#fc0505'
+    });
+
+    $("#rateYo1").rateYo({
+        starWidth: "20px",
+        rating: 5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    
+    $("#rateYo2").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo3").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo4").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    
+    $("#rateYo5").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo6").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+      $("#rateYo7").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    
+    $("#rateYo8").rateYo({
+        starWidth: "20px",
+        rating: 3.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo9").rateYo({
+        starWidth: "20px",
+        rating: 4,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo10").rateYo({
+        starWidth: "20px",
+        rating: 5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    
+    $("#rateYo11").rateYo({
+        starWidth: "20px",
+        rating: 5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+
+    $("#rateYo12").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      }); 
+    $("#rateYo13").rateYo({
+        starWidth: "20px",
+        rating: 1.0,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    $("#rateYo14").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    $("#rateYo15").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    $("#rateYo16").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+    $("#rateYo17").rateYo({
+        starWidth: "20px",
+        rating: 4.5,
+        ratedFill:"#faff00",
+        readOnly:"True"
+      });
+});
